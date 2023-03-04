a222089bfdf4631bc04b46c88e679bd836859716
Mohd Imran Khan <79039055+codingbaba1@users.noreply.github.com>
2023-03-05 00:07:17 +0530

    Add files via upload

---
+++ src/main/java/tv/ProductbycollectionApplication.java
@@ -0,0 +1,12 @@
+package tv.oled;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductbycollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductbycollectionApplication.class, args);
+	}
+
+}
